b1e3fbc553c5a670de17fc69d58ad6b0bd81e107
Tauseef Ansari <tauseeftanvir@gmail.com>
2023-06-23 12:19:10 +0530

    Fixed assets urls

--- src/Helpers/Helpers.ts
+++ src/Helpers/Helpers.ts
@@ -1,6 +1,3 @@
-import { PROJECT_NAME } from "Helpers/constants";
-
 export const imagePath = (image: string) => {
-  const env = import.meta.env.PROD ? `${PROJECT_NAME}` : "";
-  return `${env}/assets/images/${image}`;
+  return `/assets/images/${image}`;
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,26 @@
-import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react';
-import viteTsconfigPaths from 'vite-tsconfig-paths';
-import svgrPlugin from 'vite-plugin-svgr';
-import { PROJECT_NAME } from './src/Helpers/constants';
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import viteTsconfigPaths from "vite-tsconfig-paths";
+import svgrPlugin from "vite-plugin-svgr";
+import { PROJECT_NAME } from "./src/Helpers/constants";
 
 export default defineConfig(({ command }) => {
-  const base = command === 'serve' ? '/' : `/${PROJECT_NAME}/`;
+  const base = command === "serve" ? "/" : `/${PROJECT_NAME}/`;
   return {
     plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
     base,
+    assetsInclude: ["./assets/**/*"],
     test: {
       globals: true,
-      environment: 'jsdom',
-      setupFiles: ['./vitest.setup.ts']
+      environment: "jsdom",
+      setupFiles: ["./vitest.setup.ts"],
     },
     server: {
       open: true,
-      port: 3000
+      port: 3000,
     },
     build: {
-      outDir: 'build'
-    }
+      outDir: "build",
+    },
   };
 });
