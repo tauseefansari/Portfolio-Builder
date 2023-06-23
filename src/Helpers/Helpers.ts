326f90fee7233d7d3ffe58d46e9b3e8a53fbccce
Tauseef Ansari <tauseeftanvir@gmail.com>
2023-06-23 12:53:37 +0530

    Added environment conditon with log

--- src/Helpers/Helpers.ts
+++ src/Helpers/Helpers.ts
@@ -1,12 +1,7 @@
 import { PROJECT_NAME } from "./constants";
 
 export const imagePath = (image: string) => {
-  console.log(
-    image,
-    import.meta.env.PROD,
-    `/assets/images/${image}`,
-    `/${PROJECT_NAME}/assets/images/${image}`,
-    "Test all images"
-  );
-  return `${import.meta.env.PROD && `/${PROJECT_NAME}`}/assets/images/${image}`;
+  const env = import.meta.env.PROD ? `/${PROJECT_NAME}` : "";
+  console.log(env, "ENV");
+  return `${env}/assets/images/${image}`;
 };
