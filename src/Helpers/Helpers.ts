7b164777b1c76d66ba65c15f4ada6587b1ca004b
Tauseef Ansari <tauseeftanvir@gmail.com>
2023-06-23 12:44:52 +0530

    Add prod url condition

--- src/Helpers/Helpers.ts
+++ src/Helpers/Helpers.ts
@@ -1,9 +1,12 @@
+import { PROJECT_NAME } from "./constants";
+
 export const imagePath = (image: string) => {
   console.log(
     image,
     import.meta.env.PROD,
     `/assets/images/${image}`,
+    `/${PROJECT_NAME}/assets/images/${image}`,
     "Test all images"
   );
-  return `/assets/images/${image}`;
+  return `${import.meta.env.PROD && `/${PROJECT_NAME}`}/assets/images/${image}`;
 };
